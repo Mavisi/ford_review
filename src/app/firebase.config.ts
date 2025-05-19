81663f83fa22d9fbb9b2bfb069ed4848328ee3f4
Marcos Santana <marcos.vssjr@outlook.com>
2025-05-19 16:22:24 -0300

    tela de login pronta

--- src/app/firebase.config.ts
+++ src/app/firebase.config.ts
@@ -1,15 +1,14 @@
-// src/app/firebase.ts
+// src/app/firebase.config.ts
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBPxmcKIBbe8HRuS9XWY9debT7w5y8MXz0",
-  authDomain: "fordreviews-cde42.firebaseapp.com",
-  projectId: "fordreviews-cde42",
-  storageBucket: "fordreviews-cde42.appspot.com",
-  messagingSenderId: "368159832465",
-  appId: "1:368159832465:web:664474f6c33accec460230",
-  measurementId: "G-WMV84K8BY4"
+  apiKey: "AIzaSyDhWZIDarHF3zHLD50vp3wCax450VwBTrw",
+  authDomain: "fordreviewtest.firebaseapp.com",
+  projectId: "fordreviewtest",
+  storageBucket: "fordreviewtest.appspot.com", // ⬅️ Corrigido aqui (.app → .com)
+  messagingSenderId: "855820087168",
+  appId: "1:855820087168:web:d65799fbe3f697ba22db65"
 };
 
 const app = initializeApp(firebaseConfig);
