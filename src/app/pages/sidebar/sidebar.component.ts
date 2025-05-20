03f4524fb3c0c5edb5eb24a0a230edb867bc723f
Marcos Santana <marcos.vssjr@outlook.com>
2025-05-20 14:41:09 -0300

    correção de login

--- src/app/pages/sidebar/sidebar.component.ts
+++ src/app/pages/sidebar/sidebar.component.ts
@@ -1,6 +1,7 @@
-import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { filter } from 'rxjs/operators';
 
 declare const bootstrap: any;
 
@@ -11,23 +12,30 @@ declare const bootstrap: any;
   styleUrls: ['./sidebar.component.css'],
   imports: [CommonModule],
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements AfterViewInit {
+  @ViewChild('offcanvasRef') offcanvasRef!: ElementRef;
   loggedUser: any = null;
 
   constructor(private router: Router) {}
 
-  @ViewChild('offcanvasRef') offcanvasRef!: ElementRef;
+  ngAfterViewInit(): void {
+    this.loadUser();
 
-  ngOnInit(): void {
-    // Carrega o usuário logado assim que o componente for inicializado
+    // Atualiza o usuário sempre que navegar entre páginas
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.loadUser();
+    });
+  }
+
+  loadUser() {
     this.loggedUser = JSON.parse(localStorage.getItem('loggedUser') || 'null');
   }
 
   logoutAndClose() {
     localStorage.removeItem('loggedUser');
-    const bsOffcanvas = bootstrap.Offcanvas.getInstance(
-      this.offcanvasRef.nativeElement
-    );
+    const bsOffcanvas = bootstrap.Offcanvas.getInstance(this.offcanvasRef.nativeElement);
     if (bsOffcanvas) {
       bsOffcanvas.hide();
     }
@@ -36,9 +44,7 @@ export class SidebarComponent implements OnInit {
 
   navigateAndClose(path: string) {
     this.router.navigate([path]);
-    const bsOffcanvas = bootstrap.Offcanvas.getInstance(
-      this.offcanvasRef.nativeElement
-    );
+    const bsOffcanvas = bootstrap.Offcanvas.getInstance(this.offcanvasRef.nativeElement);
     if (bsOffcanvas) {
       bsOffcanvas.hide();
     }
